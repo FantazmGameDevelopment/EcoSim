50855ee3b045ddd6e35e00f5ad6570fcd75caada
Koen van Lin <koenvanlin@home.nl>
2014-08-21 11:42:29 +0200

    Marker action window update
    it now shows the selected amount of markers. This wasn't implemented at
    all.

--- Assets/Scripts/GameCtrl/GameButtons/MarkerActionWindow.cs
+++ Assets/Scripts/GameCtrl/GameButtons/MarkerActionWindow.cs
@@ -15,6 +15,9 @@ namespace Ecosim.GameCtrl.GameButtons
 		private readonly string dialogText;
 		private readonly string shortText;
 		private readonly string costStr;
+		private string totalCostStr;
+		private string totalMarkersStr;
+		private long totalCost = -1L;
 		
 		private static Texture2D tickbox;
 		private static Texture2D tickboxEmpty;
@@ -35,17 +38,23 @@ namespace Ecosim.GameCtrl.GameButtons
 		
 		public override void Render ()
 		{
+			if (totalCost != ui.estimatedTotalCostForYear) {
+				totalCost = ui.estimatedTotalCostForYear;
+				int nrMarkers = (ui.cost == 0)?0:((int) (totalCost / ui.cost));
+				totalCostStr = totalCost.ToString ("#,##0\\.-", CultureInfo.GetCultureInfo ("en-GB"));
+				totalMarkersStr = nrMarkers.ToString ("#,##0", CultureInfo.GetCultureInfo ("en-GB"));
+			}
+
 			SimpleGUI.Label (new Rect (xOffset + 65, yOffset, winWidth - 65, 32), ui.name, title);
 			SimpleGUI.Label (new Rect (xOffset, yOffset + 33, winWidth, 65), ui.description, formatted);
 			SimpleGUI.Label (new Rect (xOffset, yOffset + 99, 263, 32), "Selected locations", entry);
-			SimpleGUI.Label (new Rect (xOffset + 264, yOffset + 99, 88, 32), "0", entry);
+			SimpleGUI.Label (new Rect (xOffset + 264, yOffset + 99, 88, 32), totalMarkersStr, entry);
 			SimpleGUI.Label (new Rect (xOffset + 353, yOffset + 99, 32, 32), "", entry);
 			SimpleGUI.Label (new Rect (xOffset, yOffset + 132, 263, 32), "Cost per location", entry);
 			SimpleGUI.Label (new Rect (xOffset + 264, yOffset + 132, 88, 32), costStr, entry);
 			SimpleGUI.Label (new Rect (xOffset + 353, yOffset + 132, 32, 32), "x", entry);
 			SimpleGUI.Label (new Rect (xOffset, yOffset + 165, 263, 32), "Total cost", entry);
-			SimpleGUI.Label (new Rect (xOffset + 264, yOffset + 165, 88, 32),
-				ui.estimatedTotalCostForYear.ToString ("#,##0\\.-", CultureInfo.GetCultureInfo ("en-GB")), entry);
+			SimpleGUI.Label (new Rect (xOffset + 264, yOffset + 165, 88, 32), totalCostStr, entry);
 			SimpleGUI.Label (new Rect (xOffset + 353, yOffset + 165, 32, 32), "=", entry);
 			SimpleGUI.Label (new Rect (xOffset, yOffset + 198, 261, 32), "", header);
 			if (SimpleGUI.Button (new Rect (xOffset + 262, yOffset + 198, winWidth - 262, 32), "Accept", entry, entrySelected)) {
